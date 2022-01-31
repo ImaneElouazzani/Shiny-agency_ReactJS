780e57f53ac32f8b1f5968c7712012b1018b2411
Imane Elouazzani <denjala.smiley@gmail.com>
2022-01-31 20:22:21 +0100

    utilisation de react router v6 mais en vain

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-// import {Routes, Route} from 'react-router-dom'
-// import Home from './Pages/Home/Home'
-// import Survey from './Pages/Survey/Survey'
 
 ReactDOM.render(
   <React.StrictMode>
-   
     <App/>
   </React.StrictMode>,
   document.getElementById('root')

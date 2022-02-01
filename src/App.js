7ec780551cf1fbad134f24e8b5d0e9054bf48272
Imane Elouazzani <denjala.smiley@gmail.com>
2022-02-01 18:23:16 +0100

    questionnaire terminé

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import Home from './Pages/Home/Home';
 import Survey from './Pages/Survey/Survey'
 import {Routes, Route} from 'react-router-dom'
+import Header from './Components/Header/Header';
+import Freelances from './Pages/Freelances/Freelances'
+import Results from './Pages/Results/Results'
+import Error from './Components/Error/Error';
 
 export default function App() {
   return <div>
+      <Header/>
       <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/survey" element={<Survey/>}/>
+      <Route path="/survey/:questionNumber" element={<Survey/>}/>
+      <Route path="/results" element={<Results/>}/>
+      <Route path="/freelances" element={<Freelances/>}/>
+      <Route path="*" element={<Error/>}/>
     </Routes>
   </div>;
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import {Link} from 'react-router-dom'
+
+export default function Header() {
+  return <nav>
+      <Link to="/">Accueil</Link>
+      <Link to="/survey/1">Questionnaire</Link>
+      <Link to="/freelances">Profils</Link>
+  </nav>;
+}

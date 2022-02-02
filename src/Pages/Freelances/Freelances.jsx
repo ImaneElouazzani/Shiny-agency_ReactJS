f68e5639b85fa96eb1e0ce2603475d508fb96786
Imane Elouazzani <denjala.smiley@gmail.com>
2022-02-02 06:15:23 +0100

    ajout des profils freelance + typage avec propTypes

--- src/Pages/Freelances/Freelances.jsx
+++ src/Pages/Freelances/Freelances.jsx
@@ -1,7 +1,30 @@
+import Card from '../../Components/Card/Card'
 
+const freelanceProfiles = [
+  {
+    name: 'Jane Doe',
+    jobTitle: 'Devops',
+  },
+  {
+    name: 'John Doe',
+    jobTitle: 'Developpeur frontend',
+  },
+  {
+    name: 'Jane Biche',
+    jobTitle: 'Développeuse Fullstack',
+  },
+]
 
 export default function Freelances() {
   return <div>
       <h1>Freelances 👩‍💻👨‍💻👩‍💻</h1>
+      {freelanceProfiles.map((profile, index) => (
+        <Card
+          key={`${profile.name}-${index}`}
+          label={profile.jobTitle}
+          title={profile.name}
+        />
+      )) 
+      }
   </div>;
 }

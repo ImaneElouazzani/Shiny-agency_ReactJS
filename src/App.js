1361dbd94eb469e343c79b02e60268fd1fe63538
Imane Elouazzani <denjala.smiley@gmail.com>
2022-02-02 21:25:30 +0100

    styliser les composants avec styledcomponent

--- src/App.js
+++ src/App.js
@@ -6,11 +6,23 @@ import Header from './Components/Header/Header';
 import Freelances from './Pages/Freelances/Freelances'
 import Results from './Pages/Results/Results'
 import Error from './Components/Error/Error';
+import { createGlobalStyle } from 'styled-components'
+
+
+const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+    body {
+      margin: 0;
+    }
+`
 
 export default function App() {
   return <div>
-      <Header/>
-      <Routes>
+    <GlobalStyle />
+    <Header/>
+    <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/survey/:questionNumber" element={<Survey/>}/>
       <Route path="/results" element={<Results/>}/>
--- src/Components/Error/Error.jsx
+++ src/Components/Error/Error.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+import svgError from '../../assets/undraw.svg'
 
-export default function Error() {
-  return <div>
-      <h1> Oups 🙈 Cette page n'existe pas</h1>
-  </div>;
+const ErrorWrapper = styled.div`
+  margin: 30px;
+  display: flex;
+  flex-direction: column;
+  background-color: ${colors.background};
+  align-items: center;
+`
+
+const ErrorTitle = styled.h1`
+  font-weight: 300;
+`
+
+const ErrorSubtitle = styled.h2`
+  font-weight: 300;
+  color: ${colors.secondary};
+`
+
+const Illustration = styled.img`
+  max-width: 800px;
+`
+
+function Error() {
+  return (
+    <ErrorWrapper>
+      <ErrorTitle>Oups...</ErrorTitle>
+      <Illustration src= {svgError} />
+      <ErrorSubtitle>
+        Il semblerait que la page que vous cherchez n’existe pas
+      </ErrorSubtitle>
+    </ErrorWrapper>
+  )
 }
+
+export default Error
